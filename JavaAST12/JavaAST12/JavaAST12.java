c9f558c0a91fccecdd6ad26dedabfac18871491d
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-09-18 13:17:42 -0300

    wip: entrada de dados

--- JavaAST12/JavaAST12/JavaAST12.java
+++ JavaAST12/JavaAST12/JavaAST12.java
@@ -1,4 +1,21 @@
 package JavaAST12;
 
+import java.util.Scanner;
+
 public class JavaAST12 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do primeiro usuário: ");
+        String nomeUsuario1 = scanner.nextLine().trim();
+
+        System.out.print("Digite o nome do segundo usuário: ");
+        String nomeUsuario2 = scanner.nextLine().trim();
+
+        Chat chat = new Chat(nomeUsuario1, nomeUsuario2);
+
+        scanner.close();
+    }
 }
